eb724b3fde598d9bc8dde2cb9de46d6e99437c32
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-09-07 20:42:55 +0000

    Remove trailing whitespace

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,19 +2,18 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
     ln = len(sys.argv)
     if ln != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-     
+
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
 
     if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op == '-':       
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
