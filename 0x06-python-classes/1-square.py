a00b7635f8e1d7d8d4cd28a0b66ba003b83811b3
TheCodeMonk99 <rugutkclinton@gmail.com>
2024-04-23 10:35:35 +0300

    Fix second and third tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/in/python3
+"""Defines a class square"""
+
+class Square:
+    """This represents a square"""
+
+    def __init__(self, size):
+        """This initializes size of square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
