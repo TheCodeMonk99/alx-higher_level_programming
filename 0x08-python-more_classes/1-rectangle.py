bfdd5b6512d7956b38f34481611633c873038be8
TheCodeMonk99 <rugutkclinton@gmail.com>
2024-04-24 13:46:46 +0300

    Sing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This defines rectangles"""
+
+"""This defines rectangles absolutely"""
 
 
 class Rectangle:
@@ -38,48 +39,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of the rectangle"""
-#!/usr/bin/python3
-"""Defines a Rectangle class."""
-
-
-class Rectangle:
-    """Represent a rectangle."""
-
-    def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Get/set the width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Get/set the height of the rectangle."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
