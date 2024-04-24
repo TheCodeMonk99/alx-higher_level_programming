93036766c328eb37451a9c3c4fff3217c43a7bef
TheCodeMonk99 <rugutkclinton@gmail.com>
2024-04-24 11:52:37 +0300

    Sing

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,8 +35,14 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) for num in value) or not all(num >= 0 for num in value)):
-               raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not all(isinstance(num, int) for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not all(num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
---
+++ 0x06-python-classes/name.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+name = input("What is your name?")
+age = input ("What is your age?")
+print("Your name is {} and you are {} years old".format(name, age))
