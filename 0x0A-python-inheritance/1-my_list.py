dff63d1f024fb28edfec4fc31f70ff74b1e568fb
TheCodeMonk99 <rugutkclinton@gmail.com>
2024-04-30 00:10:49 +0300

    What are these??

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Class from list"""
+"""Class is documented and inherited from list"""
 
 
 class MyList(list):
     """This list is superb"""
+
     def __init__(self):
     """Initializes object"""
+
     super().__init__()    
+
     def print_sorted(self):
+
         """Prints a sorted list"""
         print(sorted(self))
