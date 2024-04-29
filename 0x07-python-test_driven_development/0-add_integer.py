bd0a2652e2df26f99dfc4904ca151287c6c198be
TheCodeMonk99 <rugutkclinton@gmail.com>
2024-04-29 11:24:46 +0300

    Add

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines integer additon"""
+
+
+def add_integer(a, b=98):
+    """ Sums a and b
+    
+    Args:
+        a (int): first int
+        b (int): second int
+    
+    Float: converts float to int
+    Raises: TypeError: If either of a or b is non-interger and
+    non-foat
+    """
+    if ((not isinstance(a, int)) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int)) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
