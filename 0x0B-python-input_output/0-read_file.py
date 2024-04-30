f21be519878ea75b7ec3cc7fefccae4b82b227eb
TheCodeMonk99 <rugutkclinton@gmail.com>
2024-04-30 13:10:41 +0300

    Task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a module that deals with input output"""
+
+
+def read_file(filename=""):
+    """A function that reads a text file (UTF8) and prints it
+    to standard output
+    Args:
+        filename: the name of the file
+    """
+    with open(filename, 'r', encoding="utf8") as f:
+        for line in f:
+           print(line, end='')
